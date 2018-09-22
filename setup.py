b6b50a494c34d5104e414e41e5bbe5c2d4bf7075
Mikołaj "Natsuyasumi" Kuranowski <mkuranowski@gmail.com>
2018-09-22 23:15:41 +0200

    1.2 release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   name = "pyroutelib3",
   py_modules = ["pyroutelib3"],
   license = "GPL v3",
-  version = "1.1",
+  version = "1.2",
   description = "Library for simple routing on OSM data",
   long_description = readme,
   long_description_content_type = "text/markdown",
@@ -15,7 +15,7 @@ setup(
   maintainer = "Mikolaj Kuranowski",
   maintainer_email = "mkuranowski@gmail.com",
   url = "https://github.com/MKuranowski/pyroutelib3",
-  download_url = "https://github.com/MKuranowski/pyroutelib3/archive/1.1.tar.gz",
+  download_url = "https://github.com/MKuranowski/pyroutelib3/archive/1.2.tar.gz",
   keywords = "osm routing pyroutelib",
   classifiers = [],
   install_requires = ["osmapi", "python-dateutil"]
