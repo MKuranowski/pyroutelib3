691af7f4791545a160be18292b1a9cd97faa46a4
Mikołaj Kuranowski <mkuranowski@gmail.com>
2024-07-25 11:06:57 +0200

    Adaptors for networkx graphs

---
+++ pyroutelib3/nx.py
@@ -0,0 +1,159 @@
+from dataclasses import dataclass
+from operator import itemgetter
+from typing import Any, Callable, Iterable, Mapping, Optional, Protocol, Tuple
+
+from .protocols import GraphLike as OurGraphLike
+from .protocols import Position
+
+"""The ``nx`` module contains helpers for adapting
+`networkx graphs <https://networkx.org/documentation/stable/reference/introduction.html#graphs>`_
+into the interfaces expected by pyroutelib3.
+
+To adapt a graph, use :py:class:`ExternalGraphAdaptor` or :py:class:`GraphAdaptor` with
+the appropriate attribute getters. The networkx graph must use integers as nodes.
+"""
+
+Data = Mapping[str, Any]
+"""Data represents arbitrary networkx data held in a dictionary."""
+
+
+class NodeViewLike(Protocol):
+    """NodeViewLike describes the minimal set of features required from
+    `nx.Graph.nodes <https://networkx.org/documentation/stable/reference/classes/generated/networkx.Graph.nodes.html#networkx.Graph.nodes>`_
+    to adapt the nodes of a networkx graph.
+    """
+
+    def __getitem__(self, __o: int) -> Data: ...
+
+
+class AtlasViewLike(Protocol):
+    """AtlasViewLike describes the minimal set of features required from the return value of
+    calling ``get`` on
+    `nx.Graph.adj <https://networkx.org/documentation/stable/reference/classes/generated/networkx.Graph.adj.html#networkx.Graph.adj>`_
+    to adapt the edges of a networkx graph.
+    """
+
+    def items(self) -> Iterable[Tuple[int, Data]]: ...
+
+
+class AdjacencyViewLike(Protocol):
+    """AdjacencyViewLike describes the minimal set of features required from
+    `nx.Graph.adj <https://networkx.org/documentation/stable/reference/classes/generated/networkx.Graph.adj.html#networkx.Graph.adj>`_
+    to adapt the edges of a networkx graph.
+    """
+
+    def get(self, __o: int) -> Optional[AtlasViewLike]: ...
+
+
+class GraphLike(Protocol):
+    """GraphLike describes the minimal set of features required from a
+    `nx.Graph <https://networkx.org/documentation/stable/reference/classes/graph.html>`_
+    to adapt it into the :py:class:`pyroutelib3.GraphLike` protocol.
+    """
+
+    @property
+    def nodes(self) -> NodeViewLike: ...
+
+    @property
+    def adj(self) -> AdjacencyViewLike: ...
+
+
+@dataclass
+class NodeAdaptor:
+    """NodeAdaptor adapts a networkx node into the :py:class:`pyroutelib3.NodeLike`
+    protocol.
+    """
+
+    id: int
+    data: Data
+
+    position_getter: Callable[[Data], Position] = itemgetter("lat", "lon")
+    """position_getter is used to extract the :py:obj:`Position` of a node
+    from its networkx data dictionary. Defaults to returning ``(data["lat"], data["lon"])``.
+
+    Usage of `operator.itemgetter <https://docs.python.org/3/library/operator.html#operator.itemgetter>`_
+    is highly recommended.
+    """
+
+    @property
+    def position(self) -> Position:
+        return self.position_getter(self.data)
+
+
+@dataclass
+class GraphAdaptor(OurGraphLike[NodeAdaptor]):
+    """GraphAdaptor adapts a networkx graph into the :py:class:`pyroutelib3.GraphLike`
+    protocol, with plain nodes **without** external ids.
+    """
+
+    g: GraphLike
+
+    edge_cost_getter: Callable[[Data], float] = itemgetter("weight")
+    """node_position_getter is used to extract the cost/weight of an edge
+    from its networkx data dictionary. Defaults to returning ``data["weight"]``.
+
+    Usage of `operator.itemgetter <https://docs.python.org/3/library/operator.html#operator.itemgetter>`_
+    is highly recommended.
+    """
+
+    node_position_getter: Callable[[Data], Position] = itemgetter("lat", "lon")
+    """node_position_getter is used to extract the :py:obj:`Position` of a node
+    from its networkx data dictionary. Defaults to returning ``(data["lat"], data["lon"])``.
+
+    Usage of `operator.itemgetter <https://docs.python.org/3/library/operator.html#operator.itemgetter>`_
+    is highly recommended.
+    """
+
+    def get_node(self, id: int) -> NodeAdaptor:
+        return NodeAdaptor(
+            id=id,
+            data=self.g.nodes[id],
+            position_getter=self.node_position_getter,
+        )
+
+    def get_edges(self, id: int) -> Iterable[Tuple[int, float]]:
+        if atlas := self.g.adj.get(id):
+            for to_id, edge_data in atlas.items():
+                yield to_id, self.edge_cost_getter(edge_data)
+
+
+@dataclass
+class ExternalNodeAdaptor(NodeAdaptor):
+    """NodeAdaptor adapts a networkx node into the :py:class:`pyroutelib3.ExternalNodeLike`
+    protocol.
+    """
+
+    external_id_getter: Callable[[Data], int] = itemgetter("external_id")
+    """external_id_getter is used to extract the external ID of a node from its networkx data
+    dictionary. Defaults to returning ``data["external_id"]``.
+
+    Usage of `operator.itemgetter <https://docs.python.org/3/library/operator.html#operator.itemgetter>`_
+    is highly recommended.
+    """
+
+    @property
+    def external_id(self) -> int:
+        return self.external_id_getter(self.data)
+
+
+@dataclass
+class ExternalGraphAdaptor(GraphAdaptor, OurGraphLike[ExternalNodeAdaptor]):
+    """ExternalGraphAdaptor adapts a networkx graph into the :py:class:`pyroutelib3.GraphLike`
+    protocol, with nodes containing external IDs.
+    """
+
+    node_external_id_getter: Callable[[Data], int] = itemgetter("external_id")
+    """node_external_id_getter is used to extract the external ID of a node from its networkx data
+    dictionary. Defaults to returning ``data["external_id"]``.
+
+    Usage of `operator.itemgetter <https://docs.python.org/3/library/operator.html#operator.itemgetter>`_
+    is highly recommended.
+    """
+
+    def get_node(self, id: int) -> ExternalNodeAdaptor:
+        return ExternalNodeAdaptor(
+            id=id,
+            data=self.g.nodes[id],
+            position_getter=self.node_position_getter,
+            external_id_getter=self.node_external_id_getter,
+        )
---
+++ pyroutelib3/test_nx.py
@@ -0,0 +1,61 @@
+from dataclasses import dataclass
+from operator import itemgetter
+from typing import Any, Dict
+from unittest import TestCase
+
+from .nx import ExternalGraphAdaptor, GraphAdaptor
+
+
+@dataclass
+class NxDiGraphMock:
+    nodes: Dict[int, Dict[str, Any]]
+    adj: Dict[int, Dict[int, Dict[str, Any]]]
+
+
+class TestNetworkxAdaptors(TestCase):
+    def setUp(self) -> None:
+        #  (20)  (20)  (20)
+        # 1─────2─────3─────4
+        #       └─────5─────┘
+        #        (10)   (10)
+        self.g = NxDiGraphMock(
+            nodes={
+                1: {"x": 1, "y": 1, "external_id": 101},
+                2: {"x": 2, "y": 1, "external_id": 102},
+                3: {"x": 3, "y": 1, "external_id": 103},
+                4: {"x": 4, "y": 1, "external_id": 104},
+                5: {"x": 3, "y": 0, "external_id": 105},
+            },
+            adj={
+                1: {2: {"weight": 20}},
+                2: {1: {"weight": 20}, 3: {"weight": 20}, 5: {"weight": 10}},
+                3: {2: {"weight": 20}, 4: {"weight": 20}},
+                4: {3: {"weight": 20}, 5: {"weight": 10}},
+                5: {2: {"weight": 10}, 4: {"weight": 10}},
+            },
+        )
+
+    def test_plain(self) -> None:
+        a = GraphAdaptor(self.g, node_position_getter=itemgetter("x", "y"))
+
+        n = a.get_node(3)
+        self.assertEqual(n.id, 3)
+        self.assertEqual(n.position, (3, 1))
+
+        with self.assertRaises(KeyError):
+            a.get_node(42)
+
+        self.assertSetEqual(set(a.get_edges(2)), {(1, 20), (3, 20), (5, 10)})
+
+    def test_external(self) -> None:
+        a = ExternalGraphAdaptor(self.g, node_position_getter=itemgetter("x", "y"))
+
+        n = a.get_node(3)
+        self.assertEqual(n.id, 3)
+        self.assertEqual(n.external_id, 103)
+        self.assertEqual(n.position, (3, 1))
+
+        with self.assertRaises(KeyError):
+            a.get_node(42)
+
+        self.assertSetEqual(set(a.get_edges(2)), {(1, 20), (3, 20), (5, 10)})
